9149df45962447ed7e5fdc0bc896f4f902edb455
aikenm <a.mnsbkv@gmail.com>
2023-07-29 22:06:33 +0600

    Implemented categorizing, + added comments

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
-let addTask = document.querySelector('.taskInput'),
-    addButton = document.querySelector('.add'),
-    todo = document.querySelector('.taskList'),
-    businessBtn = document.querySelector('.option1'),
-    personalBtn = document.querySelector('.option2'),
-    editBtn = document.querySelector('.edit-button'),
-    deleteBtn = document.querySelector('.delete-button')
-
+const addTask = document.querySelector('.taskInput');
+const addButton = document.querySelector('.add');
+const todo = document.querySelector('.taskList');
 const editableName = document.getElementById('editableName');
 const todoListElement = document.getElementById('todoList');
+const businessCheckbox = document.getElementById('businessRadio');
+const personalCheckbox = document.getElementById('personalRadio');
 
 let todoList = [];
 
+// Function for making the user name editable
 editableName.addEventListener('click', function () {
     this.setAttribute('contentEditable', 'true');
     this.focus();
 });
 
+// Function to disable the editable mode when the user name loses focus
 editableName.addEventListener('blur', function () {
     this.setAttribute('contentEditable', 'false');
 });
 
+// Function for handling the category selection of tasks
+function onCheckboxClick(selectedCategory) {
+    if (selectedCategory === 'business' && personalCheckbox.checked) {
+        personalCheckbox.checked = false;
+    } else if (selectedCategory === 'personal' && businessCheckbox.checked) {
+        businessCheckbox.checked = false;
+    }
+}
+
+// Function for adding a new task
 addButton.addEventListener('click', function () {
     const newTaskName = addTask.value.trim();
     if (newTaskName !== '') {
         const existingTaskIndex = todoList.findIndex(task => task.todo === newTaskName);
         const newTodo = {
             todo: newTaskName,
-            checked: existingTaskIndex !== -1 ? todoList[existingTaskIndex].checked : false
+            checked: existingTaskIndex !== -1 ? todoList[existingTaskIndex].checked : false,
+            category: businessRadio.checked ? 'business' : 'personal'
         };
         todoList.push(newTodo);
         displayList();
         addTask.value = '';
+
+        businessRadio.checked = false;
+        personalRadio.checked = false;
     }
 });
 
+// Function for handling clicks on the task list container
 todo.addEventListener('click', function (event) {
     const target = event.target;
     if (target.classList.contains('delete-button')) {
@@ -44,15 +58,17 @@ todo.addEventListener('click', function (event) {
     }
 });
 
+// Function for handling clicks on the task checkboxes
 todoListElement.addEventListener('click', function (event) {
     const target = event.target;
-    if (target.type === 'radio') {
+    if (target.type === 'checkbox') {
         const taskId = target.dataset.taskId;
         todoList[taskId].checked = target.checked;
         displayList();
     }
 });
 
+// Function to delete a task from the todoList array and update the display
 function deleteTask(button) {
     const li = button.parentElement;
     const taskId = li.dataset.taskId;
@@ -60,6 +76,7 @@ function deleteTask(button) {
     displayList();
 }
 
+// Function to toggle the edit mode for a task name and update the task in the todoList array
 function toggleEditTask(button) {
     const li = button.parentElement;
     const label = li.querySelector('label');
@@ -73,31 +90,59 @@ function toggleEditTask(button) {
     } else {
         label.setAttribute('contentEditable', 'true');
         label.focus();
+        const range = document.createRange();
+        const sel = window.getSelection();
+        range.selectNodeContents(label);
+        range.collapse(false);
+        sel.removeAllRanges();
+        sel.addRange(range);
     }
 }
 
+// Function to update the name of a task in the todoList array and update the display
 function updateTask(taskId, newTodoName) {
     todoList[taskId].todo = newTodoName;
+    displayList();
 }
 
-
+// Function to display the list of tasks on the webpage
 function displayList() {
-    let displayToDo = '';
+    const todoListElement = document.querySelector('.taskList');
+    todoListElement.innerHTML = '';
 
     todoList.forEach(function (item, i) {
-        displayToDo += `
-            <li data-task-id="${i}">
-                <input type='radio' id='item_${i}' data-task-id="${i}" ${item.checked ? 'checked' : ''}>
-                <label for='item_${i}' style="${item.checked ? 'text-decoration: line-through;' : ''}">${item.todo}</label>
-                <button class="edit-button">Edit</button>
-                <button class="delete-button">Delete</button>
-            </li>
-            `;
+        const categoryClass = item.category === 'business' ? 'business-border' : 'personal-border';
+        const checkedAttribute = item.checked ? 'checked' : '';
+
+        const li = document.createElement('li');
+        li.dataset.taskId = i;
+        li.classList.add(categoryClass);
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = `item_${i}`;
+        checkbox.dataset.taskId = i;
+        checkbox.classList.add('custom-checkbox');
+        checkbox.checked = item.checked;
+
+        const label = document.createElement('label');
+        label.htmlFor = `item_${i}`;
+        label.style.textDecoration = item.checked ? 'line-through' : '';
+        label.textContent = item.todo;
+
+        const editButton = document.createElement('button');
+        editButton.classList.add('edit-button');
+        editButton.textContent = 'Edit';
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-button');
+        deleteButton.textContent = 'Delete';
+
+        li.appendChild(checkbox);
+        li.appendChild(label);
+        li.appendChild(editButton);
+        li.appendChild(deleteButton);
+
+        todoListElement.appendChild(li);
     });
-
-    todo.innerHTML = displayToDo;
-};
-
-
-
-
+}
